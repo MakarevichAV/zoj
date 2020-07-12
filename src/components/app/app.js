4ad44cfee01f0ba51e6b28d2a72a1c1a54972991
MakarevichAV <makarevich.a.v@yandex.ru>
2020-07-13 01:35:56 +0300

    Верстка страницы Входа

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,117 +13,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default class App extends Component {
 
-    // const minWeight = 86; // будет расчитываться
-    // const maxWeight = 96; // будет расчитываться
-    // const optimalWeight = 92; // будет расчитываться
-
-    // const normsInfo = [
-    //     { 
-    //         key: 'Норма веса',
-    //         value: minWeight + ' - ' + maxWeight,
-    //         unit: 'кг',
-    //         underline: true,
-    //         change: false,
-    //         important: true,
-    //         head: false
-    //     },
-    //     { 
-    //         key: 'Оптимальный вес',
-    //         value: optimalWeight,
-    //         unit: 'кг',
-    //         underline: true,
-    //         change: false,
-    //         important: true,
-    //         head: false
-    //     },
-    // ];
-
-    // const adviceInfo = [
-    //     { 
-    //         key: 'Суточная норма',
-    //         value: '',
-    //         underline: false,
-    //         change: false,
-    //         important: false,
-    //         head: true
-    //     },
-    //     { 
-    //         key: 'Калории',
-    //         value: '2430',
-    //         unit: 'кКал/день',
-    //         underline: true,
-    //         change: false,
-    //         important: false,
-    //         head: false
-    //     },
-    //     { 
-    //         key: 'Белки',
-    //         value: '60',
-    //         unit: 'г/день',
-    //         underline: true,
-    //         change: false,
-    //         important: false,
-    //         head: false
-    //     },
-    //     { 
-    //         key: 'Жиры',
-    //         value: '30',
-    //         unit: 'г/день',
-    //         underline: true,
-    //         change: false,
-    //         important: false,
-    //         head: false
-    //     },
-    //     { 
-    //         key: 'Углеводы',
-    //         value: '40',
-    //         unit: 'г/день',
-    //         underline: true,
-    //         change: false,
-    //         important: false,
-    //         head: false
-    //     },
-    //     { 
-    //         key: 'Вода',
-    //         value: '3.4',
-    //         unit: 'л/день',
-    //         underline: true,
-    //         change: false,
-    //         important: false,
-    //         head: false
-    //     }
-    // ];
-
-    // const userInfo = [
-    //     { 
-    //         key: 'Возраст',
-    //         value: '31',
-    //         unit: 'год',
-    //         underline: false,
-    //         change: false,
-    //         important: false,
-    //         head: false
-    //     },
-    //     { 
-    //         key: 'Рост',
-    //         value: '189',
-    //         unit: 'см',
-    //         underline: false,
-    //         change: false,
-    //         important: false,
-    //         head: false
-    //     },
-    //     { 
-    //         key: 'Вес',
-    //         value: '105',
-    //         unit: 'кг',
-    //         underline: false,
-    //         change: false,
-    //         important: false,
-    //         head: false
-    //     }
-    // ];
-
     state = {
         isLoggedIn: false
     };
@@ -136,6 +25,119 @@ export default class App extends Component {
 
     render () {
 
+        // Временное хранилище --НАЧАЛО--
+                const minWeight = 86; // будет расчитываться
+                const maxWeight = 96; // будет расчитываться
+                const optimalWeight = 92; // будет расчитываться
+
+                const normsInfo = [
+                    { 
+                        key: 'Норма веса',
+                        value: minWeight + ' - ' + maxWeight,
+                        unit: 'кг',
+                        underline: true,
+                        change: false,
+                        important: true,
+                        head: false
+                    },
+                    { 
+                        key: 'Оптимальный вес',
+                        value: optimalWeight,
+                        unit: 'кг',
+                        underline: true,
+                        change: false,
+                        important: true,
+                        head: false
+                    },
+                ];
+
+                const adviceInfo = [
+                    { 
+                        key: 'Суточная норма',
+                        value: '',
+                        underline: false,
+                        change: false,
+                        important: false,
+                        head: true
+                    },
+                    { 
+                        key: 'Калории',
+                        value: '2430',
+                        unit: 'кКал/день',
+                        underline: true,
+                        change: false,
+                        important: false,
+                        head: false
+                    },
+                    { 
+                        key: 'Белки',
+                        value: '60',
+                        unit: 'г/день',
+                        underline: true,
+                        change: false,
+                        important: false,
+                        head: false
+                    },
+                    { 
+                        key: 'Жиры',
+                        value: '30',
+                        unit: 'г/день',
+                        underline: true,
+                        change: false,
+                        important: false,
+                        head: false
+                    },
+                    { 
+                        key: 'Углеводы',
+                        value: '40',
+                        unit: 'г/день',
+                        underline: true,
+                        change: false,
+                        important: false,
+                        head: false
+                    },
+                    { 
+                        key: 'Вода',
+                        value: '3.4',
+                        unit: 'л/день',
+                        underline: true,
+                        change: false,
+                        important: false,
+                        head: false
+                    }
+                ];
+
+                const userInfo = [
+                    { 
+                        key: 'Возраст',
+                        value: '31',
+                        unit: 'год',
+                        underline: false,
+                        change: false,
+                        important: false,
+                        head: false
+                    },
+                    { 
+                        key: 'Рост',
+                        value: '189',
+                        unit: 'см',
+                        underline: false,
+                        change: false,
+                        important: false,
+                        head: false
+                    },
+                    { 
+                        key: 'Вес',
+                        value: '105',
+                        unit: 'кг',
+                        underline: false,
+                        change: false,
+                        important: false,
+                        head: false
+                    }
+                ];
+        // Временное хранилище --КОНЕЦ--
+
         const { isLoggedIn } = this.state;
 
         if (isLoggedIn) {  // Если пользователь залогинен
@@ -146,18 +148,18 @@ export default class App extends Component {
         
                         <Route  path="/" 
                                 render= {() =>  <ProfilePage 
-                                                    // userInfo={userInfo} 
-                                                    // normsInfo={normsInfo} 
-                                                    // adviceInfo={adviceInfo}
+                                                    userInfo={userInfo} 
+                                                    normsInfo={normsInfo} 
+                                                    adviceInfo={adviceInfo}
                                                 />
                                         }
                                 exact/>
                                 
                         <Route  path="/profile" 
                                 render= {() =>  <ProfilePage 
-                                                    // userInfo={userInfo} 
-                                                    // normsInfo={normsInfo} 
-                                                    // adviceInfo={adviceInfo}
+                                                    userInfo={userInfo} 
+                                                    normsInfo={normsInfo} 
+                                                    adviceInfo={adviceInfo}
                                                 />}/>
                         <Route path="/food" component={FoodPage} />
                         <Route path="/sport" component={SportPage} />
--- src/components/pages/login-page/login-page.js
+++ src/components/pages/login-page/login-page.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 import './login-page.css';
 
+import Logo from '../../header/logo';
+
 const LoginPage = () => {
     return (
-        <h1>Это страница Входа</h1>
+        <div className="login-page">
+            <div className="login-window">
+                <div className="login-header">
+                    <Logo />
+                </div>
+                <div className="login-nav">
+                    <a href="#" className="nav-item here">Вход</a>
+                    <a href="#" className="nav-item">Регистрация</a>
+                </div>
+                <div className="login-form">
+                    <form action="#" method="POST">
+                        <input type="text" className="login-input" placeholder="Логин"/>
+                        <input type="password" className="login-input" placeholder="Пароль"/>
+                        <button type="submit" className="login-button">ВОЙТИ</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
